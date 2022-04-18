d0ad48233f57576647b6109bcdce8407edfd7a73
Tolga Altinordu <tolgaaltinordu@gmail.com>
2022-04-18 21:42:31 +0300

    grass changed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let randomCard, firstCard, secondCard
+let randomCard, firstCard, secondCard, newCard, cardT
 let deck = [
     "1s", "2s", "3s", "4s", "5s", "6s", "7s", "8s", "9s", "10s", "11js", "12qs", "13ks",
     "1h", "2h", "3h", "4h", "5h", "6h", "7h", "8h", "9h", "10h", "11jh", "12qh", "13kh",
@@ -10,12 +10,10 @@ let player = {
     chips: 50,
     sayHello: function () {
         console.log("hello")
+        document.getElementById('player-el').innerText = player.name + ": " + player.chips + " TL"
     }
 }
-// player.sayHello()
-// let tag = document.createElement('p')
-// let text = document.createTextNode("dsadasdas")
-// tag.appendChild(text)
+player.sayHello()
 let noCardsOntheTable = true
 let isGameStart = false
 let sum = 0
@@ -25,23 +23,119 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.querySelector('#sum-el')
 let cardsEl = document.querySelector('#cards-el')
-let playerEl = document.getElementById('player-el')
+let element = document.querySelector('.box2')
+document.getElementById("new-card-btn").style.visibility = "hidden"
 
 /* Start Game  */
 function startGame() {
-    isAlive = true
-    renderGame()
+    if (isGameStart === false) {  // sadece gamestatr true ise start game buttonu çalşacak
+        let startBtn = document.getElementById("start-btn").style.visibility = "hidden"
+        document.getElementById("new-card-btn").style.visibility = "visible"
+        isAlive = true
+        isGameStart = true
+        firstCard = getRandomCard()
+        secondCard = getRandomCard()
+        console.log(firstCard, secondCard)
+        /* first to cards on the table */
+        if (noCardsOntheTable === true) {
+            noCardsOntheTable = false
+            for (let i = 0; i < 2; i++) {
+
+                if (i === 0 && firstCard.indexOf("s") !== -1) {
+
+                    let cardT = document.createElement("card-t")
+                    cardT.setAttribute("suit", "spades")
+                    cardT.setAttribute("rank", parseInt(firstCard))
+                    element.appendChild(cardT)
+                } else if (i === 0 && firstCard.indexOf("h") !== -1) {
+
+                    let cardT = document.createElement("card-t")
+                    cardT.setAttribute("suit", "hearts")
+                    cardT.setAttribute("rank", parseInt(firstCard))
+                    element.appendChild(cardT)
+                } else if (i === 0 && firstCard.indexOf("d") !== -1) {
+
+                    let cardT = document.createElement("card-t")
+                    cardT.setAttribute("suit", "diamonds")
+                    cardT.setAttribute("rank", parseInt(firstCard))
+                    element.appendChild(cardT)
+                } else if (i === 0 && firstCard.indexOf("c") !== -1) {
+
+                    let cardT = document.createElement("card-t")
+                    cardT.setAttribute("suit", "clubs")
+                    cardT.setAttribute("rank", parseInt(firstCard))
+                    element.appendChild(cardT)
+                } else if (i === 1 && secondCard.indexOf("s") !== -1) {
+
+                    let cardT = document.createElement("card-t")
+                    cardT.setAttribute("suit", "spades")
+                    cardT.setAttribute("rank", parseInt(secondCard))
+                    element.appendChild(cardT)
+                } else if (i === 1 && secondCard.indexOf("h") !== -1) {
+
+                    let cardT = document.createElement("card-t")
+                    cardT.setAttribute("suit", "hearts")
+                    cardT.setAttribute("rank", parseInt(secondCard))
+                    element.appendChild(cardT)
+                } else if (i === 1 && secondCard.indexOf("d") !== -1) {
+
+                    let cardT = document.createElement("card-t")
+                    cardT.setAttribute("suit", "diamonds")
+                    cardT.setAttribute("rank", parseInt(secondCard))
+                    element.appendChild(cardT)
+                } else if (i === 1 && secondCard.indexOf("c") !== -1) {
+
+                    let cardT = document.createElement("card-t")
+                    cardT.setAttribute("suit", "clubs")
+                    cardT.setAttribute("rank", parseInt(secondCard))
+                    element.appendChild(cardT)
+                }
+            }
+        }
+        /* first to cards on the table - end */
+        firstCard = parseInt(firstCard)
+        secondCard = parseInt(secondCard)
+        console.log(secondCard)
+        console.log(firstCard)
+        if (firstCard >= 11) {
+            firstCard = 10
+        }
+        if (secondCard >= 11) {
+            secondCard = 10
+        }
+        console.log("firstcard:", firstCard, "second:card", secondCard)
+        sum = firstCard + secondCard
+
+        renderGame()
+
+    }
 }
 /* Start Game - end */
 
 /* Render Game */
 function renderGame() {
-    firstCard = getRandomCard()
-    secondCard = getRandomCard()
-    for (let i = 0; i < 2; i++) {
-        if (i === 0 && firstCard === firstCard.indexOf("s")) {
 
-        }
+    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    if (sum < 21) {
+        message = "Do you wanna draw a new card ?"
+        messageEl.style.color = "goldenrod"
+        messageEl.textContent = message
+    } else if (sum === 21) {
+        message = "You've got blackjack"
+        messageEl.style.color = "blue"
+        messageEl.textContent = message
+        hasBlackJack = true
+        isAlive = false
+        document.getElementById("start-btn").style.visibility = "visible"
+        document.getElementById("start-btn").innerText = "PLAY AGAIN"
+    } else {
+        message = "You've lost"
+        messageEl.style.color = "darkred"
+        messageEl.textContent = message
+        isAlive = false
+        document.getElementById("start-btn").style.visibility = "visible"
+        document.getElementById("start-btn").innerText = "PLAY AGAIN"
     }
 }
 /* Render Game - end */
@@ -53,3 +147,60 @@ function getRandomCard() {
     return deck[randomCard]
 }
 /* getRandomCard - end */
+
+/* new card */
+function getNewCard() {
+    if (isAlive !== false) {
+        newCard = getRandomCard()
+        sum += parseInt(newCard)
+        console.log("newcard: ", newCard, "sum: ", sum)
+        if (newCard.indexOf("s") !== -1) {
+            cardT = document.createElement("card-t")
+            cardT.setAttribute("suit", "spades")
+            cardT.setAttribute("rank", parseInt(newCard))
+            element.appendChild(cardT)
+        } else if (newCard.indexOf('h') !== -1) {
+            cardT = document.createElement("card-t")
+            cardT.setAttribute("suit", "hearts")
+            cardT.setAttribute("rank", parseInt(newCard))
+            element.appendChild(cardT)
+        } else if (newCard.indexOf("d") !== -1) {
+            cardT = document.createElement("card-t")
+            cardT.setAttribute("suit", "diamonds")
+            cardT.setAttribute("rank", parseInt(newCard))
+            element.appendChild(cardT)
+        } else if (newCard.indexOf("c") !== -1) {
+            cardT = document.createElement("card-t")
+            cardT.setAttribute("suit", "clubs")
+            cardT.setAttribute("rank", parseInt(newCard))
+            element.appendChild(cardT)
+        }
+        renderGame()
+    }
+
+}
+/* new card -end */
+
+/* play agian */
+function playAgain() {
+    if (isAlive === false && isGameStart === true) {
+        sumEl.textContent = "sum:"
+        cardsEl.textContent = "Cards:"
+        messageEl.style.color = "#fff"
+        messageEl.textContent = "Want to play a round ?"
+        isGameStart = false
+        hasBlackJack = false
+        noCardsOntheTable = true
+        document.getElementById("start-btn").innerText = "START GAME"
+        document.getElementById("new-card-btn").style.visibility = "hidden"
+        //remove allchilds
+        removeAllChildNodes()
+        function removeAllChildNodes(parent) {
+            while (element.firstChild) {
+                element.removeChild(element.firstChild);
+            }
+        }
+        //remove allchilds
+    }
+}
+/* play agian - end */
